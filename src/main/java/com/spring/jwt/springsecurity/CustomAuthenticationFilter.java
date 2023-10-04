450510f5c081c2b87271f200294244d6fa43a03f
YangHeoSu <com6474@gmail.com>
2023-10-04 13:43:33 +0900

    fix : FormLogin 시 아이디, 비밀번호 인증 수정

--- src/main/java/com/spring/jwt/springsecurity/CustomAuthenticationFilter.java
+++ src/main/java/com/spring/jwt/springsecurity/CustomAuthenticationFilter.java
@@ -8,9 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // UsernamePasswordAuthenticationFilter -> Spring Security에서 formLogin을 할 때 사용할 수 있는 Filter.
@@ -19,11 +23,27 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-        ObjectMapper objectMapper = new ObjectMapper();
+        String userId = request.getParameter("userId"); // 로그인 아이디
+        String password = request.getParameter("password"); // 로그인 비밀번호
+
+        CustomUserDetail customUserDetail = (CustomUserDetail) userDetailsService.loadUserByUsername(userId);
+
+        if (customUserDetail.getUsername() == null) {
+            throw new UsernameNotFoundException("UserId is Empty");
+        }
+        if (!bCryptPasswordEncoder.matches(customUserDetail.getPassword(), password)) {
+            throw new BadCredentialsException("비밀번호 틀림");
+        }
+
+        return new UsernamePasswordAuthenticationToken(customUserDetail, null, customUserDetail.getAuthorities());
+
+/*        ObjectMapper objectMapper = new ObjectMapper();
         LoginDTO loginDTO = new LoginDTO();
         try {
              loginDTO = objectMapper.readValue(request.getInputStream(), LoginDTO.class);
@@ -33,6 +53,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginDTO.getUserId(), loginDTO.getPassword());
 
-        return authenticationManager.authenticate(authenticationToken);
+        return authenticationManager.authenticate(authenticationToken);*/
     }
 }
