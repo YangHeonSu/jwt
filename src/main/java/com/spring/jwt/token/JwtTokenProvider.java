d3e70a77f10b3711e3d81376c8c4e81a49c8c286
didgjstn585 <com6474@gmail.com>
2023-11-16 21:50:39 +0900

    fix : accessToken 생성 시 Bearer 제거
    - accessToken 생성 시 앞에 "Bearer " 제거

--- src/main/java/com/spring/jwt/token/JwtTokenProvider.java
+++ src/main/java/com/spring/jwt/token/JwtTokenProvider.java
@@ -64,7 +64,7 @@ public class JwtTokenProvider {
 
         return TokenDTO.builder()
                 .grantType("Bearer")
-                .accessToken("Bearer " + accessToken)
+                .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
     }
